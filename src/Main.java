a6d11ed27c0fae2e310fdc2da72eea198a6555f5
“Daniel <danielanderson.lopes0@gmail.com>
2024-07-03 14:12:23 -0300

    feat: adiciona resolução do primeiro exercício

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
+import entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        var sc = new Scanner(System.in);
+
+        short option;
+
+        System.out.println("""
+                Para acessar os enunciados dos exercícios, \
+
+                vá até a pasta 'documentação' e abra o arquivo PDF
+                """);
+
+        System.out.println("""
+                OPÇÃO        EXERCÍCIO
+                ----------------------
+                  1              01
+                  2              02
+                  3              03
+                ----------------------
+                """);
+
+        System.out.print("Exercício Nº: ");
+        option = sc.nextShort();
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+
+        switch(option) {
+            case 1:
+
+                System.out.println("Exercicio 01\n" +
+                                   "------------");
+
+                System.out.println("""
+
+                        Enter rectangle width and height
+                        --------------------------------""");
+
+                var rectangle = new Rectangle();
+
+                System.out.print("Width: ");
+                rectangle.width = sc.nextDouble();
+
+                System.out.print("Height: ");
+                rectangle.height = sc.nextDouble();
+
+                System.out.println(rectangle);
+
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            default:
+                System.out.println("Exercício não encontrado.");
+        }
+
+        sc.close();
     }
 }
